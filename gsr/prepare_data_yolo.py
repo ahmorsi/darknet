f19f76356d216d66c6333b2dd848043df1536f6d
ahmorsi <ahmed.mohamed.emad.morsy@gmail.com>
2019-09-08 22:53:34 +0200

    GSR commit for aws

---
+++ gsr/prepare_data_yolo.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import os
+
+width = 1360
+height = 800
+
+
+def prepare_data(base_dir, img_list_filename):
+    train_data_dict = {}
+    print("Generate Labeling Files")
+    with open(os.path.join(base_dir, "gt.txt")) as f:
+        for line in f:
+            tokens = line.split(";")
+            filename = tokens[0]
+            rest = tokens[1:]
+            if filename not in train_data_dict:
+                train_data_dict[filename] = list()
+            train_data_dict[filename].append(rest)
+
+        for filename in train_data_dict:
+            print(filename)
+            new_file_name = filename.split(".")[0] + ".txt"
+            with open(os.path.join(base_dir, new_file_name), mode="w") as fw:
+                for obj_win in train_data_dict[filename]:
+                    x1, y1, x2, y2, class_id = obj_win
+                    obj_width = int(x2) - int(x1)
+                    obj_relative_width = obj_width * 1.0 / width
+                    obj_height = int(y2) - int(y1)
+                    obj_relative_height = obj_height * 1.0 / height
+                    center_x = (int(x2) + int(x1)) * 0.5
+                    center_y = (int(y2) + int(y1)) * 0.5
+                    fw.write("{0} {1} {2} {3} {4}\n".format(class_id.strip(), center_x / width, center_y / height,
+                                                            obj_relative_width, obj_relative_height))
+
+    print("Generate list of file names")
+    with open(os.path.join(base_dir, img_list_filename), mode="w") as writer:
+        for filename in train_data_dict:
+            writer.write("{}\n".format(os.path.join(base_dir, filename)))
+
+
+base_dir = "TrainIJCNN2013/"
+img_list_filename = "train_gsr.txt"
+prepare_data(base_dir, img_list_filename)
+
+base_dir = "TestIJCNN2013/"
+img_list_filename = "test_gsr.txt"
+
+prepare_data(base_dir, img_list_filename)
---
+++ start_gsr.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install git
+sudo apt-get -y install cmake
+sudo apt-get -y install unzip
+
+git clone https://github.com/ahmorsi/darknet.git
+
+cd darknet
+cmake . && make
+
+cd gsr/
+
+wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TrainIJCNN2013.zip .
+wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TestIJCNN2013.zip .
+wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/gt.txt
+unzip TrainIJCNN2013.zip
+unzip TestIJCNN2013
+mv gt.txt TestIJCNN2013/
+python prepare_data_yolo.py
+
+mkdir -p backup
+cd ..
+wget https://pjreddie.com/media/files/darknet53.conv.74 .
+
+./darknet detector train gsr/gsr.data gsr/yolov3_gsr.cfg darknet53.conv.74 -dont_show -mjpeg_port 8090 -map
