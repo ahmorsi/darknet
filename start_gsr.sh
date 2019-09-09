c3de2ec8c76b96105ec4dccee9518df03c137e8d
uia58137 <ahmed.abdelbaki@continental-corporation.com>
2019-09-09 12:07:16 +0200

    Add OpenCV installtion

--- start_gsr.sh
+++ start_gsr.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
+sudo su
 sudo apt-get -y install git
 sudo apt-get -y install cmake
 sudo apt-get -y install unzip
 
-git clone https://github.com/ahmorsi/darknet.git
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib:/usr/local/lib
+export CPLUS_INCLUDE_PATH=/usr/local/cuda/include
+
+git clone https://github.com/opencv/opencv.git
+cd opencv/
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D WITH_CUDA=ON \
+    -D ENABLE_FAST_MATH=1 \
+    -D CUDA_FAST_MATH=1 \
+    -D WITH_CUBLAS=1 \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D BUILD_EXAMPLES=ON ..
+make
+sudo make install
+
+cd ..
 
 cd darknet
 cmake . && make
@@ -13,17 +32,17 @@ cd gsr/
 
 wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TrainIJCNN2013.zip .
 wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TestIJCNN2013.zip .
-wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/gt.txt
-unzip TrainIJCNN2013.zip
-unzip TestIJCNN2013
-mv gt.txt TestIJCNN2013/
-python prepare_data_yolo.py
+wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/gt.txt .
+unzip -q TrainIJCNN2013.zip -d ./TrainIJCNN2013/
+unzip -q TestIJCNN2013.zip -d ./TestIJCNN2013/
+mv gt.txt ./TestIJCNN2013/
 
 mkdir -p backup
 cd ..
+python gsr/prepare_data_yolo.py
 wget https://pjreddie.com/media/files/darknet53.conv.74 .
 
 ./darknet detector train gsr/gsr.data gsr/yolov3_gsr.cfg darknet53.conv.74 -dont_show -mjpeg_port 8090 -map
-aws s3 sync /local/dir s3://ai-projects-ahmorsi/yolo-gsr/
+aws s3 sync ./gsr/backup s3://ai-projects-ahmorsi/yolo-gsr/
 
 shutdown -h now
